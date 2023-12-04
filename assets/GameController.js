179ad94515c86af7b0b96c37e4cfce81e666abca
congthanh1612 <daocongthanh43@gmail.com>
2023-12-04 18:46:22 +0700

    update 1

--- assets/GameController.js
+++ assets/GameController.js
@@ -1,6 +1,12 @@
+var randomWords = require('random-words');
+const numberOfWords = 30; 
+const randomWordArray = Array.from({ length: numberOfWords }, () => randomWords());
+// console.log(randomWordArray);
+
+
 cc.Class({
     extends: cc.Component,
-
+    
     properties: {
         registerPopup: cc.Node,
         userNameInput: cc.EditBox,
@@ -11,20 +17,41 @@ cc.Class({
         userMessageInput: cc.EditBox,
         userAvatar: cc.Sprite,
         userName: cc.Label,
+        // countDown: cc.Node,
+        circleNode: cc.Sprite,
+        timeLabel: cc.Label,
+        time: 60,
+        count: 3600,
+        onCountDown: false,
+
         listAvatar: [cc.SpriteFrame],
+
+        winPopup: cc.Node,
+        score: cc.Label,
+        correctWord: cc.Label,
+
     },
 
     onLoad () {
         this.registerPopup.active = true;
         this.avatarPopup.children[0].color = cc.Color.BLUE;
         this.mainGame.active = false;
-        // cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this); 
-        // debug purpose: test main game
-        // this.mainGame.active = true;
-        // this.registerPopup.active = false;
-        // this.onEnterGame()
+        this.winPopup.active = false;
+        test
+        // this.winPopup.active = true;
+
+        this.userMessageInput.node.on('text-changed', this.onTextChanged, this);
+        this.circleNode.type = 3;
+        this.circleNode.fillType = 2;
+        this.circleNode.fillCenter.x = 0.5;
+        this.circleNode.fillCenter.y = 0.5;
+        this.circleNode.fillStart = 0.25;
+        this.circleNode.fillRange = 1;
     }, 
 
+    onTextChanged() {
+        this.onCountDown = true;
+    },
     onSelectAvatar(event, avatarOption) {
         this.avatarPopup.children.forEach((avatar, index) => {
             avatar.color = index + 1 === Number(avatarOption) ? cc.Color.BLUE : cc.Color.WHITE;
@@ -40,41 +67,90 @@ cc.Class({
             return window.alert("Your user name is less than 4 characters");
         }
         this.userInfo = {
-            avatarOption: this.avatarOption,
-            userName: this.userNameInput.string
-        }
+                            avatarOption: this.avatarOption,
+                            userName: this.userNameInput.string
+                        }
         this.startGame();
     },
 
     startGame() {
         this.registerPopup.active = false;
         this.mainGame.active = true;
+        this.winPopup.active = false;
         this.displayUserInfo();
-        setTimeout(() => {
-            if (this.textDemo.string === this.userMessageInput.string) {
-                window.alert("You Win 100 score");
-            } else {
-                window.alert("You are near win");
-            }
-        }, 15000);
+        this.calculateScore();
+
     },
-
-    // onKeyUp(event) {
-    //     switch(event.keyCode) {
-    //         case cc.macro.KEY.space:
-    //             console.log('space');
-    //             break;
-    //     }
-    // },
-
-    // onDestroy() {
-    //     cc.systemEvent.off(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
-    // },
 
     displayUserInfo() {
         const avatarOption = this.userInfo.avatarOption;
         const userName = this.userInfo.userName;
         this.userAvatar.getComponent(cc.Sprite).spriteFrame = this.listAvatar[avatarOption - 1];
-        this.userName.getComponent(cc.Label).string = `User Name: ${userName}`;
+        this.userName.getComponent(cc.Label).string = `${userName}`;
+        this.textDemo.getComponent(cc.Label).string = randomWordArray.join(" ");
+    },
+
+    update(dt) {
+        if (this.time >= 0 && this.onCountDown ) {
+            this.circleNode.fillRange -= dt / 60;
+            if (this.count % 60 === 0) {
+                this.timeLabel.string = this.time;
+                this.time--;
+            }
+            if (this.count < 3600) this.circleNode.node.color = cc.Color.GREEN;
+            if (this.count <= 3600 * 0.3) this.circleNode.node.color = cc.Color.YELLOW;
+            if (this.count <= 3600 * 0.1) this.circleNode.node.color = cc.Color.RED;
+            this.count--;
+        }
+        if(this.time < 0) {
+            this.runWinPopup();
+        }
+    },
+
+    runWinPopup(){
+        this.registerPopup.active = false;
+        this.mainGame.active = false;
+        this.winPopup.active = true;
+        
+        const correctWords = this.countCorrectWords();
+        const totalWords = randomWordArray.length;
+        const percentage = (correctWords / totalWords) * 100;
+
+        const score = this.calculateScore(correctWords);
+        this.displayScore(score,correctWords);
+
+        // const score = this.calculateScore
+        // this.calculateScore();
+        // this.userScore = {
+        //         score: this.score,
+        //         countCorrectWords: this.correctWords
+        //     }
+
+        // this.displayScore();
+    },
+
+    calculateScore() {
+        const correctWords = this.countCorrectWords();
+        const totalWords = randomWordArray.length;
+        const percentage = (correctWords / totalWords) * 100;
+    },
+
+    countCorrectWords() {
+        const typedWords = this.userMessageInput.string.trim().split(/\s+/);        
+        const demoWords = this.textDemo.string.trim().split(/\s+/);
+
+        let correctCount = 0;
+
+        for (let i = 0; i < typedWords.length; i++) {
+            if (typedWords[i] === demoWords[i]) {
+                correctCount++;
+            }
+        }
+        return correctCount;
+    },
+
+    displayScore (){
+        this.score.getComponent(cc.Label).string = `Your Score: ${score}`;
+        this.correctWord.getComponent(cc.Label).string = `Corrected Words: ${countCorrectWords}`;
     }
-});
+}); 
